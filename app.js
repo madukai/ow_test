8963934d3562246301c1bfc102d5ce80a6d26501
Joji <noypako@hotmail.com>
2018-12-27 21:10:30 -0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var nodemailer = require('nodemailer');
+
+var app = express();
+var count = 0;
+
+app.set('view engine', 'html');
+app.engine('html', require('ejs').renderFile);
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function (req, res) {
+    count++;
+    
+    res.render(GetHTMLString('webpageapp'), {req_count: count});
+});
+
+app.get('/math', function (req, res) {
+    res.render(GetHTMLString('math'));
+});
+
+app.get('/game', function (req, res) {
+    
+    res.render(GetHTMLString('game'));
+});
+
+app.listen(3000);
+console.log('Server is running on port 3000');
+
+
+function GetHTMLString(str) 
+{
+    let htmlFile = str + '.html';
+
+    return htmlFile;
+}
---
+++ public/scripts/math_script.js
@@ -0,0 +1,9 @@
+
+var btn = document.getElementsByClassName("widget_button");
+
+btn[0].addEventListener('click', widgetBtnClick);
+
+function widgetBtnClick() {
+    console.log("WIDGET");
+}
+
